b28f680d81ac5c99bae39a480c277bd73d133148
Alaafathalla <alaafathalla36@gmail.com>
2024-07-30 06:39:50 +0300

    first

---
+++ src/Components/Notes/Notes.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function (){
+  return (
+    <div>
+
+<div className="col-10 px-lg-5 px-2 py-5">
+            <div className='text-end me-2'>
+            <button className='btn btn-primary text-white '><i className="fa-solid fa-plus"></i> Add Note</button>
+            </div>
+          <h2 className='text-muted fw-bold mt-4'>My Notes</h2>
+          <hr />
+          <div className="row mt-5">
+
+          </div>
+          
+        </div>
+
+    </div>
+  )
+}
